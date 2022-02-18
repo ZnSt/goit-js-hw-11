a231d400578ab43851242be75f3281142f886a24
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2022-02-18 23:04:27 +0200

    выполнил д/з

--- src/index.js
+++ src/index.js
@@ -6,31 +6,47 @@ const refs = {
   formSearch: document.querySelector('#search-form'),
   inputSearch: document.querySelector('[name="searchQuery"]'),
   listGallery: document.querySelector('.list'),
+  btnGallery: document.querySelector('.btn-render'),
 };
 
-refs.formSearch.addEventListener('submit', searchImg);
-
+let page = 1;
 let inputValue = '';
-console.log(fetchData);
 
-function searchImg(events) {
+const searchImg = async events => {
+  btnClassAdd();
+  clearPage();
   events.preventDefault();
   inputValue = events.currentTarget.elements.searchQuery.value.trim();
 
   if (!inputValue) {
-    clearPage();
     return;
   }
-  fetchData(inputValue).then(response => {
+  page = 1;
+  try {
+    const response = await fetchData(inputValue);
+
+    if (!response.hits.length) {
+      Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      return;
+    }
+
+    btnClassRemove();
     renderGallery(response.hits);
-  });
-}
+  } catch (error) {
+    Notify.failure(error.message);
+  }
+
+  updateBtn();
+};
+
+refs.formSearch.addEventListener('submit', searchImg);
+
 function renderGallery(array) {
   const markup = array
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `
           <a href="${largeImageURL}" class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300"/>
   <div class="info">
     <p class="info-item">
       <b>Likes</b>${likes}
@@ -46,7 +62,7 @@ function renderGallery(array) {
     </p>
   </div>
 </a>
-          `;
+        `;
     })
     .join('');
   refs.listGallery.insertAdjacentHTML('beforeend', markup);
@@ -54,4 +70,36 @@ function renderGallery(array) {
 
 function clearPage() {
   refs.listGallery.innerHTML = '';
+}
+
+refs.btnGallery.addEventListener('click', newPage);
+
+async function newPage() {
+  page += 1;
+  updateBtn(true);
+
+  try {
+    const response = await fetchData(inputValue, page);
+
+    renderGallery(response.hits);
+    if (response.hits.length < 40) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      btnClassAdd();
+    }
+  } catch (error) {
+    Notify.failure(error.message);
+  }
+  updateBtn();
+}
+
+function btnClassRemove() {
+  refs.btnGallery.classList.remove('is-hidden');
+}
+
+function btnClassAdd() {
+  refs.btnGallery.classList.add('is-hidden');
+}
+
+function updateBtn(toggle = false) {
+  refs.btnGallery.disabled = toggle;
 }
--- src/servises/api.js
+++ src/servises/api.js
@@ -2,14 +2,20 @@ import axios from 'axios';
 const API_KEY = '25755107-c5ecbaee54c3d5c87c2809c98';
 const BASE_URL = 'https://pixabay.com/api/';
 
-function fetchData(name) {
-  return axios
-    .get(
-      `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`,
-    )
-    .then(response => {
-      return response.data;
-    });
-}
+// function fetchData(name, page = 1) {
+//   return axios
+//     .get(
+//       `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`,
+//     )
+//     .then(response => {
+//       return response.data;
+//     });
+// }
 
+const fetchData = async (name, page = 1) => {
+  const response = await axios.get(
+    `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`,
+  );
+  return response.data;
+};
 export { fetchData };
